dc2df730005436660600e6564985f5dcb99e50af
xiaoshizis <3439163714@qq.com>
2021-03-08 15:30:32 +0800

    add ViewModel and LiveData

---
+++ app/src/main/java/com/example/gittestfinally/MyViewModel.java
@@ -0,0 +1,26 @@
+package com.example.gittestfinally;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.material.badge.BadgeDrawable;
+
+public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> number ;
+
+    public MyViewModel() {
+        if (number==null) {
+            number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+    }
+
+    public LiveData<Integer> getNumber() {
+        return number;
+    }
+
+    public void add(int n) {
+        number.setValue(number.getValue() + n);
+    }
+}
